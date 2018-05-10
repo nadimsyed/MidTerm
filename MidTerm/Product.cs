15471b50e95e6b32dcce2b65be883c82cb0fd2f9
Adam Horn <Adam@Adam-Horns-MacBook-Pro.local>
2018-05-10 15:54:15 -0400

    added menu selection logic to main program method

--- MidTerm/Product.cs
+++ MidTerm/Product.cs
@@ -13,48 +13,7 @@ namespace MidTerm
 
 		public static string Dairy()
         {
-			//List<Product> ReadFile(string filename)
-            ArrayList Products = new ArrayList();
 
-            StreamReader sr = null;
-            try
-            {
-                sr = new StreamReader("Dairy.txt");
-
-                while (!sr.EndOfStream)
-                {
-                    String line = sr.ReadLine();
-
-					string x = line.Split('\t');
-
-                    //Product p = new Product(line, "", "", 0);
-                    //Products.Add(p);
-
-                    /*
-                    if (line == null)
-                    {
-                        Console.WriteLine("End of file");
-                        return;
-                    }
-                    */
-
-                    Console.WriteLine("read: " + line);
-                }
-                Console.WriteLine("End of file");
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Error reading file");
-            }
-            finally
-            {
-                if (sr != null)
-                {
-                    sr.Close();
-                }
-            }
-
-            return Products;
         }
     }
 }
