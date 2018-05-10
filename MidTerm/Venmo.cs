4afeda131c9a93fb718ff91afcc32ec76a01bc7c
Adam Horn <Adam@Adam-Horns-MacBook-Pro.local>
2018-05-10 14:21:21 -0400

    added Cash and Venmo classes

--- MidTerm/Venmo.cs
+++ MidTerm/Venmo.cs
@@ -2,11 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MidTerm
 {
     class Venmo
     {
+		public static void PayCash()
+        {
+            Console.WriteLine("Great you are paying with Venmo!");
+
+            while (true)
+			{
+				Console.WriteLine("What is your venmo username?");
+				string VenmoUsername = Console.ReadLine();
+
+
+				if (Regex.IsMatch(VenmoUsername, "^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$"))
+				{
+					Console.WriteLine($"Thanks, your Venmo account {VenmoUsername} has been charged!");
+					break;
+				}
+				else
+				{
+					Console.WriteLine("Sorry I didn't get that.");
+					Console.WriteLine("Please try again");
+				}
+            }
+        }
     }
 }
